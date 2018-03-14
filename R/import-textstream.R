371c531ed01568ba608bc8b10ea3eb62c73380bb
isaacverm <isaacverm@gmail.com>
2018-03-14 13:19:25 +0000

    Add import-textstream functions

--- R/import-textstream.R
+++ R/import-textstream.R
@@ -1,7 +1,47 @@
 get_textstream <- function(matchid) {
+  resource <- paste("fixtures", matchid, "textstream/EN", sep = "/")
 
+  parameters <- list(pageSize = 1000)
+
+  response <- get_premier_league(resource,
+                                 parameters)
+
+  return(response)
 }
 
-extract_events <- function(textstream_json) {
+extract_match_details <- function(response) {
+  body <- content(response)
 
+  extract_event_details <- function(raw_event) { # raw meaning containing superfluous info
+    parsed_event <- list()
+
+    parsed_event$time <- raw_event[["time"]][["secs"]]
+    parsed_event$type <- raw_event[["type"]]
+    parsed_event$player_id <- as.character(raw_event[["playerIds"]])
+
+    # make sure all events have the same fields
+    # e.g. time field doesn't exist for certain types (for example. doesn't make sense for line-up)
+    add_missing_fields <- function(parsed_event) {
+      necessary_fields <- c("time","type","player_id")
+      missing_fields <- setdiff(necessary_fields, names(parsed_event))
+
+      parsed_event[missing_fields] <- NA
+
+      return(parsed_event)
+    }
+
+    event <- add_missing_fields(parsed_event)
+
+    return(event)
+  }
+
+  match_details <- list()
+  match_details$gameweek <- as.character(body[["fixture"]][["gameweek"]][["gameweek"]])
+  match_details$attendance <- as.character(body[["fixture"]][["attendance"]])
+
+  raw_events <- body[["events"]][["content"]]
+  match_details$events <- raw_events %>%
+    map(extract_event_details)
+
+  return(match_details)
 }
---
+++ tests/testthat/test-import-textstream.R
@@ -0,0 +1,73 @@
+context("get_textstream")
+
+test_that("returns response succesfully", {
+  response <- get_textstream(matchid = "22632")
+  
+  expect_equal(response$status_code, 200)
+  expect_is(response, "response")
+})
+
+context("extract_match_details")
+
+test_that("returns named list", {
+  response <- get_textstream(matchid = "22632")
+  match_details <- extract_match_details(response)
+  
+  expect_is(match_details, "list")
+  expect_is(names(match_details), "character")
+})
+
+test_that("returns gameweek, attendance and events", {
+  response <- get_textstream(matchid = "22632")
+  match_details <- extract_match_details(response)
+  
+  expect_equal(names(match_details), c("gameweek","attendance","events"))
+  
+})
+
+test_that("events contains time and type of event + id player involved", {
+  # setup test
+  response <- get_textstream(matchid = "22632")
+  match_details <- extract_match_details(response)
+  
+  # setup expectations
+  id_regex <- "\\d{4,5}"
+  events <- match_details$events
+  max_length_match <- 90*60 + 10*60 # maximum length match is 90*60 seconds (regular time) + 10*60 seconds (overtime) 
+  
+  # content expectations
+  # events %>%
+  #   walk(~expect_named(., c("time","type","player_id"))) # makes conceptually the most sense but is very slow
+  # instead the calculate the frequency of each field and see if total adds up
+  expected_fields <- c("time","type","player_id")
+  freq_fields_list <- events %>% 
+    map(~names(.))
+  freq_fields_table <- table(unlist(freq_fields_list))
+  
+  nr_actual_fields <- sum(freq_fields_table)
+  nr_expected_fields <- length(events) * length(expected_fields)
+  
+  expect_equal(nr_actual_fields, nr_expected_fields)
+    
+  # player_id expectation
+  player_ids <- events %>%
+    map(~.[["player_id"]])
+  player_ids <- unlist(player_ids)
+  
+  expect_match(player_ids, id_regex)
+  
+  # time of event expectation
+  time <- events %>% 
+    map(~.[["time"]])
+  time <- unlist(time)
+  
+  higher_than_min_length_match <- all(time >= 0, na.rm = TRUE)
+  lower_than_max_length_match <- all(time < max_length_match, na.rm = TRUE)
+  expect_true(higher_than_min_length_match & lower_than_max_length_match)
+  
+  # type of event expectation
+  type <- events %>% 
+    map(~.[["type"]])
+  type <- unlist(type)
+  expect_is(type, "character") # not feasible to list all types of match events
+})
