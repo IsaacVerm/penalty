daa6d7a466e51a26a7fcc11b626e16299298f84d
isaacverm <isaacverm@gmail.com>
2018-03-15 15:28:57 +0000

    Add import save_match_details function

--- R/import-helpers.R
+++ R/import-helpers.R
@@ -1,8 +1,44 @@
 # do the absolute minimum to have your data in dataframe format
-save_match_details <- function(match_details) {
+# list as argument because you want to save a lot of matches in a single dataframe
+save_match_details <- function(list_match_details) {
+  extract_single_value <- function(field, list) {
+    value <- list %>% map(~.[[field]])
+    
+    return(value)
+  }
   
+  extract_event_values <- function(field, list_match_details) {
+    values <- list_match_details %>%
+      map(~extract_single_value(list = .[["events"]],
+                                field))
+    values <- flatten(values)
+          
+    return(values)
+  }
+  
+  replace_empty_with_na <- function(list) {
+    empty_ind <- sapply(list, function(x) is_empty(x))
+    list[empty_ind] <- NA
+    return(list)
+  }
+  
+  df_event_details <- data.frame(type_of_event = unlist(extract_event_values("type", list_match_details)),
+                                 time_of_event = unlist(extract_event_values("time", list_match_details)))
+  df_event_details$player_id <- replace_empty_with_na(
+    extract_event_values("player_id", list_match_details)
+  ) # list within list because multiple players can be involved in the same event
+  
+  nr_events_by_match <- list_match_details %>% map_int(~length(.[["events"]]))
+  df_single_value_details <- data.frame(gameweek = unlist(extract_single_value("gameweek", list_match_details)),
+                                        attendance = unlist(extract_single_value("attendance", list_match_details)))
+  df_single_value_details <- df_single_value_details %>% 
+    tidyr::uncount(weights = nr_events_by_match)
+  
+  df_match_details <- dplyr::bind_cols(df_single_value_details, df_event_details)
+  
+  save(df_match_details, file = paste(here(),"data","import-df_match_details.RData", sep = "/"))
 }
 
-save_player_ids <- function(player_ids) {
+save_player_ids <- function(list_player_ids) {
   
 }
--- tests/testthat/test-import-helpers.R
+++ tests/testthat/test-import-helpers.R
@@ -1,16 +1,51 @@
+# setup common to all tests
+if (length(ls()) > 0) {
+  rm(list = ls()) # remove objects in working directory (if any)
+}
+
+project_root <- here()
+data_path <- paste(project_root,"data", sep = "/") # variables used by all tests
+
+setup_files_to_remove <- dir(path = data_path,
+                             pattern = "import*")
+if (length(setup_files_to_remove) > 0) {
+  file.remove(paste(data_path, setup_files_to_remove, sep = "/")) # remove files already saved before (if any)
+}
+
 context("save_match_details")
 
-test_that("file is saved in project_root/data", {
+# setup
+match_ids <- c("22342","22344")
+responses <- match_ids %>%  map(get_textstream)
+list_match_details <- responses %>% map(extract_match_details) # get list of match_details
+
+save_match_details(list_match_details)
+load(paste(data_path, "import-df_match_details.RData", sep = "/")) # save and load back into memory
+
+# run tests
+test_that("file import-df_match_details.RData is saved in project_root/data", {
+  saved_files <- dir(data_path)
+  
+  expect_true("import-df_match_details.RData" %in% saved_files)
 })
 
-test_that("file is called import_match_details" , {
+test_that("df_match_details saved is dataframe", {
+  expect_is(df_match_details, 'data.frame')
 })
 
-test_that("file saved is dataframe", {
+test_that("df_match_details has gameweek, attendance, type_of_event, player_id and time_of_event columns", {
+  expect_named(df_match_details, c("gameweek", "attendance", "type_of_event", "time_of_event", "player_id"))
+})
+
+test_that("df_match_details isnt't empty", {
+  expect_true(nrow(df_match_details) > 1) # if  you put placeholder 0 would still pass
 })
 
 context("save_player_ids")
 
+# setup
+
+# run tests
 test_that("file is saved in project_root/data", {
 })
 
@@ -19,3 +54,11 @@ test_that("file is called save_player_ids" , {
 
 test_that("file saved is dataframe", {
 })
+
+# clean
+clean_files_to_remove <- dir(path = data_path,
+                             pattern = "import*")
+file.remove(paste(data_path, clean_files_to_remove, sep = "/"))
+
+rm(list = ls()) # remove objects in working directory
+
